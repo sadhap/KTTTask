e355fd711c785c8d4dd04fb664d10249f33435bc
M.SADHASIVAM <69914346+sadhap@users.noreply.github.com>
2026-07-03 11:56:03 +0530

    Update index.js
    Removed my personal mongoDB URI

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGO_URI || 'mongodb+srv://sadhasivam:aFg6BuymtUZ7V7r1@sadhacluster.agcnpf2.mongodb.net/taskmanager?retryWrites=true&w=majority';
+const uri = process.env.MONGO_URI || '';
 mongoose.connect(uri)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
